80fb07688c217c55c9566705a2e8810509e4c1a1
ShreyaKumari13 <shreyakumari2713@gmail.com>
2024-12-24 18:02:21 +0530

    Simplify medusa configuration and remove plugins

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -1,56 +1,30 @@
 const dotenv = require('dotenv')
 
-let ENV_FILE_NAME = '';
-switch (process.env.NODE_ENV) {
-  case 'production':
-    ENV_FILE_NAME = '.env.production';
-    break;
-  case 'staging':
-    ENV_FILE_NAME = '.env.staging';
-    break;
-  case 'test':
-    ENV_FILE_NAME = '.env.test';
-    break;
-  case 'development':
-  default:
-    ENV_FILE_NAME = '.env';
-    break;
-}
-
+let ENV_FILE_NAME = '.env';
 try {
   dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
 } catch (e) {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = process.env.ADMIN_CORS || "*";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "*";
 
-// Database URL (here we use a local database called medusa-development)
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
-
-// Medusa uses Redis, so this needs to be set
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
-
-const plugins = [
-  `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
-];
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 
 module.exports = {
   projectConfig: {
-    redis_url: REDIS_URL,
     database_url: DATABASE_URL,
     database_type: "postgres",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
-    database_extra: { ssl: { rejectUnauthorized: false } },
+    database_extra: { 
+      ssl: { 
+        rejectUnauthorized: false 
+      } 
+    },
   },
-  plugins,
-  featureFlags: {
-    admin_dashboard: false,
-  },
+  plugins: [],
 };
